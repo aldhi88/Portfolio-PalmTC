df927e581308b08dfce87ea5e6ab57678ee7726d
Aldhi <aldhi.albadri@gmail.com>
2025-02-02 11:45:26 +0700

    fix dashboad sample

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
 {
     public function index(){
-        $data['title'] = "Dashboard";
+        $data['title'] = "Dashboard Summary";
         $data['desc'] = "Display all summary data in statistical format";
         return view('modules.dashboard.index', compact('data'));
     }
